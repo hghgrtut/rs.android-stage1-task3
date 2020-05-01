cef1e7e68e98f6900b2a6c223949be1f9c31a044
hghgrtut <hghghghg.hh39@gmail.com>
2020-05-01 21:08:54 +0300

    feat: 1 task

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,32 @@
 package subtask1
 
+import kotlin.math.abs
+
 class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.isEmpty()) return null
+        var degr = numbers.lastIndex
+        var polynomial=numbers[0].toString()+degree(degr)
+        for (i in 1..numbers.lastIndex){
+            degr--
+            if (numbers[i]>0) { polynomial +=
+                    if (i==numbers.lastIndex || numbers[i]>1) " + ${numbers[i]}${degree(degr)}"
+                    else " + ${degree(degr)}"
+            } else if (numbers[i]<0) { polynomial +=
+                    if (i==numbers.lastIndex || numbers[i]<-1) " - ${abs(numbers[i])}${degree(degr)}"
+                    else " - ${degree(degr)}"
+            }
+        }
+        return polynomial
+    }
+
+    private fun degree(degr: Int): String{
+         return when(degr){
+             0 -> ""
+             1 -> "x"
+             else ->"x^$degr"
+         }
     }
 }
