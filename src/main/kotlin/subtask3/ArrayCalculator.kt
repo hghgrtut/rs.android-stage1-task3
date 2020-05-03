ff7f0860f8fcc33db354c5c868bec22c56e926d4
hghgrtut <hghghghg.hh39@gmail.com>
2020-05-03 19:16:03 +0300

    feat: 3 task

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -2,8 +2,29 @@ package subtask3
 
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var n=numberOfItems
+        val integerArray: ArrayList<Int> = ArrayList()
+        var product=1
+        for (item in itemsFromArray) if (item is Int) integerArray.add(item)
+        if (integerArray.size==0) {product=0}
+        else if (integerArray.size<=n) {integerArray.forEach { num -> product*=num }}
+        else {
+            integerArray.sort()
+            while (n>1){
+                if (integerArray[0]*integerArray[1]>integerArray[integerArray.lastIndex]*integerArray[integerArray.lastIndex-1]) {
+                    product*=integerArray[0]*integerArray[1]
+                    integerArray.removeAt(0)
+                    integerArray.removeAt(1)
+                } else{
+                    product*=integerArray[integerArray.lastIndex]*integerArray[integerArray.lastIndex-1]
+                    integerArray.removeAt(integerArray.lastIndex-1)
+                    integerArray.removeAt(integerArray.lastIndex)
+                }
+                n-=2
+            }
+            if (n==1){product*= if (integerArray[0]>integerArray.last()) integerArray[0] else integerArray.last()}
+        }
+        return product
     }
 }
