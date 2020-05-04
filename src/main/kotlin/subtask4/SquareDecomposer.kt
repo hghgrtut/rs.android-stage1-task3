c2e1719822e1f00ad26c831ab1915cb5cc8cef4d
Hg Hg <hghghghg.hh39@gmail.com>
2020-05-04 19:32:34 +0300

    feat: task 4 for small numbers

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,34 @@
 package subtask4
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val ish=decompose(number*number, number)
+        if (ish==null) return null
+        else {
+            val a: Array<Int>? = Array(ish.size-1){0}
+            for (i in a!!.indices) a[i]=ish[i+1]
+            return a
+        }
+    }
+
+    private fun decompose(sum: Int, number: Int): ArrayList<Int>? {
+            if (sum<0) return null
+            else if (sum==0) return arrayListOf(number)
+            else {
+                var n=number-1
+                var arl: ArrayList<Int>? =null
+                while (n>0 && arl ==null){
+                    arl=decompose(sum-n*n, n)
+                    n--
+                }
+                if (arl == null) return null
+                else{
+                    arl.add(n+1)
+                    return arl
+                }
+            }
     }
 }
