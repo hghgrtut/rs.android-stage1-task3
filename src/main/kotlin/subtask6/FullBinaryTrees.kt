0f01e43941454eb209af0ce13d02412f52e6ba60
Hg Hg <hghghghg.hh39@gmail.com>
2020-05-05 21:20:09 +0300

    feat: 6 task

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,19 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        var result ="["
+        if (count%2==1) for (tree in newstr("[0", count-1, 0)) result+=tree
+        result+="]"
+        return result
+    }
+
+    private fun newstr(nach: String, symb: Int, free: Int): ArrayList<String>{
+        if (symb==0) return arrayListOf("$nach]")
+        else {
+            val arr = newstr("$nach,0,0", symb - 2, free+1)
+            if (free>0) arr.addAll(newstr("$nach,null,null", symb, free-1))
+            return arr
+        }
     }
 }
